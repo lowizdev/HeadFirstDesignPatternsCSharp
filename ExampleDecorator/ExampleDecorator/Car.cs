89e403b910497653e8312045eeab9996d5e312ba
PirateKaiju <santosgonsalvesjr@gmail.com>
2020-03-10 21:07:15 -0300

    Decorator Pattern

---
+++ ExampleDecorator/ExampleDecorator/Car.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleDecorator
+{
+    abstract class Car
+    {
+
+        protected string model = "any model";
+        protected double price = 0;
+
+        public abstract double getPrice();
+
+        public abstract string getModel();
+
+    }
+
+    abstract class Decorator : Car {
+
+        protected Car decoratedCar;
+
+        public Decorator(Car decoratedCarInst) {
+
+            this.decoratedCar = decoratedCarInst;
+        
+        }
+
+    }
+
+    class Tuner : Car {
+
+        public Tuner() {
+
+            this.model = "A Tuner car";
+            this.price = 4000;
+        
+        }
+
+        public override string getModel()
+        {
+            return this.model;
+        }
+
+
+        public override double getPrice() {
+
+            return this.price;
+        
+        }
+
+    }
+
+    class Muscle : Car
+    {
+
+        public Muscle()
+        {
+
+            this.model = "A Muscle car";
+            this.price = 3000;
+
+        }
+
+        public override string getModel()
+        {
+            return this.model;
+        }
+
+        public override double getPrice()
+        {
+
+            return this.price;
+
+        }
+
+    }
+
+    class DecoratorWithSpoiler : Decorator {
+
+        protected double spoilerPrice = 80;
+
+        public DecoratorWithSpoiler(Car decoratedCarInst): base(decoratedCarInst)
+        {
+            
+        }
+
+        public override string getModel() {
+
+            return base.decoratedCar.getModel() + " with spoilers ";
+
+        }
+
+        public override double getPrice()
+        {
+            return base.decoratedCar.getPrice() + this.spoilerPrice;
+        }
+
+
+    }
+
+    class DecoratorWithNitrous : Decorator
+    {
+
+        protected double nitrousPrice = 500;
+
+        public DecoratorWithNitrous(Car decoratedCarInst) : base(decoratedCarInst)
+        {
+
+        }
+
+        public override string getModel()
+        {
+
+            return base.decoratedCar.getModel() + " with braia's nitrous ";
+
+        }
+
+        public override double getPrice()
+        {
+            return base.decoratedCar.getPrice() + this.nitrousPrice;
+        }
+
+
+    }
+
+
+
+}
---
+++ ExampleDecorator/ExampleDecorator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExampleDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Car c1 = new Tuner();
+            Car c2 = new Muscle();
+
+            Console.WriteLine(c1.getModel());
+            Console.WriteLine(c1.getPrice());
+
+            Console.WriteLine(c2.getModel());
+            Console.WriteLine(c2.getPrice());
+
+            Decorator d1 = new DecoratorWithSpoiler(c1);
+            Decorator d2 = new DecoratorWithSpoiler(c2);
+
+            Decorator d3 = new DecoratorWithNitrous(d1);
+
+
+            Console.WriteLine(d3.getModel());
+            Console.WriteLine(d3.getPrice());
+
+            Console.WriteLine(d2.getModel());
+            Console.WriteLine(d2.getPrice());
+
+            Console.ReadLine();
+        }
+    }
+}
