1e9b7a7756667d7c763a30d197af02d00da02f52
PirateKaiju <santosgonsalvesjr@gmail.com>
2020-03-25 23:35:36 -0300

    Factory Method Pattern

---
+++ ExampleFactoryMethod/ExampleFactoryMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ExampleFactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            GuardBarracks g1 = new GuardBarracks();
+            StrategyBarracks st1 = new StrategyBarracks();
+
+            Soldier s1 = g1.trainSoldier("Elite");
+            Soldier s2 = st1.trainSoldier("Simple");
+
+        }
+    }
+}
---
+++ ExampleFactoryMethod/ExampleFactoryMethod/Soldier.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleFactoryMethod
+{
+    abstract class Soldier
+    {
+        public String weapon { get; set; }
+        public int age { get; set; }
+
+        public double salary { get; set; }
+
+        public void signUp() {
+
+            Console.WriteLine("Signing up!");
+
+        }
+
+        public abstract void equip();
+
+
+    }
+
+    class SimpleSoldier : Soldier {
+
+        public SimpleSoldier() {
+
+            this.salary = this.salary * 0.95;
+
+        }
+
+        public override void equip()
+        {
+            Console.WriteLine("Simple equipment!");
+        }
+
+    }
+
+    class EliteSoldier : Soldier {
+
+        public EliteSoldier() {
+
+            this.salary = this.salary * 1.05;
+
+        }
+
+        public override void equip() {
+
+            Console.WriteLine("With Elite equipment!");
+        }
+
+    }
+
+    abstract class Barracks {
+
+        public Soldier trainSoldier(String soldierLevel) {
+
+            Soldier s1 = this.createSoldier(soldierLevel);
+
+            s1.signUp();
+            s1.equip();
+
+            return s1;
+        
+        }
+
+        public abstract Soldier createSoldier(String level);
+    
+    }
+
+    class GuardBarracks: Barracks {
+
+        public override Soldier createSoldier(String level) {
+            Soldier soldier = null;
+
+            if (level == "Elite")
+            {
+                soldier = new EliteSoldier();
+            }
+            else if (level == "Simple")
+            {
+
+               soldier = new SimpleSoldier();
+            }
+
+            soldier.weapon = "Pike";
+            soldier.age = 30;
+            soldier.salary = 10;
+
+            return soldier;
+
+        }
+
+    }
+
+    class StrategyBarracks : Barracks
+    {
+
+        public override Soldier createSoldier(String level)
+        {
+
+            Soldier soldier = null;
+
+            if (level == "Elite")
+            {
+                soldier = new EliteSoldier();
+            }
+            else if (level == "Simple")
+            {
+
+                soldier = new SimpleSoldier();
+            }
+
+            soldier.weapon = "Dagger";
+            soldier.age = 40;
+            soldier.salary = 20;
+
+            return soldier;
+
+        }
+
+    }
+}
