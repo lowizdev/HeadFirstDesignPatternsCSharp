3b8abf649fb60099167789f5a0e7374302ad5c92
PirateKaiju <santosgonsalvesjr@gmail.com>
2020-06-24 22:50:27 -0300

    Singleton Pattern

---
+++ ExampleSingleton/ExampleSingleton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExampleSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var randomSingleton = SingleRandomSingleton.getSingleRandomSingleton();
+
+            Console.WriteLine(randomSingleton.number);
+
+            var anotherRandomSingleton  = SingleRandomSingleton.getSingleRandomSingleton();
+
+            Console.WriteLine(anotherRandomSingleton.number);
+        }
+    }
+}
---
+++ ExampleSingleton/ExampleSingleton/SingleRandomSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleSingleton
+{
+    class SingleRandomSingleton
+    {
+
+        static SingleRandomSingleton singleton = null;
+
+        public int number { get; set; }
+
+        private SingleRandomSingleton() {
+            var rdm = new Random();
+            this.number = rdm.Next(1000);
+        }
+
+        public static SingleRandomSingleton getSingleRandomSingleton() {
+
+            if (singleton == null) {
+
+                singleton = new SingleRandomSingleton();
+
+            }
+
+            return singleton;
+
+        }
+
+        
+
+    }
+}
