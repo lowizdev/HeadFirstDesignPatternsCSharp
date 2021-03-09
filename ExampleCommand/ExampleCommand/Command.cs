54cb5a4ee73ed28ff8a031fb706296f49aa763e7
PirateKaiju <santosgonsalvesjr@gmail.com>
2021-03-09 11:30:11 -0300

    Command Pattern

---
+++ ExampleCommand/ExampleCommand/Command.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleCommand
+{
+    interface Command
+    {
+        public void execute();
+    }
+
+    class CommandSave : Command
+    {
+
+        private FakeUI dest;
+        private string argument;
+
+        public CommandSave(FakeUI dest, string argument)
+        {
+            this.dest = dest;
+            this.argument = argument;
+        }
+        public void execute() {
+            Console.WriteLine("Im the save command, should be a receiver");
+            Console.WriteLine(this.dest.GetHashCode());
+        }
+
+    }
+
+    class CommandSaveShortcut : Command {
+
+        private FakeUI dest;
+        private string argument;
+
+
+        public CommandSaveShortcut(FakeUI dest, string argument)
+        {
+            this.dest = dest;
+            this.argument = argument;
+        }
+        public void execute() {
+            Console.WriteLine("Im the save command too, should be a receiver, but called from a shortcut");
+            Console.WriteLine(this.dest.GetHashCode());
+        }
+
+    }
+
+
+    class CommandDelete : Command {
+
+
+        public void execute() {
+        
+        }
+
+    }
+}
---
+++ ExampleCommand/ExampleCommand/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExampleCommand
+{
+
+    class FakeUI
+    {
+
+        //Receiver
+        public FakeUI()
+        {
+
+        }
+
+        public void render() {
+
+            Console.WriteLine("Im rendering");
+
+        }
+    }
+
+    class Invoker 
+    {
+        public Command start = null;
+        public Command finish = null;
+
+        public List<Command> history = new List<Command>();
+
+        public void run() 
+        {
+
+            Console.WriteLine("Started");
+
+            if(this.start != null){
+                this.start.execute();
+                history.Add(this.start);
+            }
+
+            Console.WriteLine("Finished");
+
+            if (this.finish != null)
+            {
+                this.finish.execute();
+                history.Add(this.finish);
+            }
+
+        }
+
+        
+
+        
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Client call
+
+            FakeUI f1 = new FakeUI();
+            CommandSave cs1 = new CommandSave(f1, "some argument");
+            CommandSaveShortcut css1 = new CommandSaveShortcut(f1, "some other argument");
+
+            Invoker inv1 = new Invoker();
+
+            inv1.start = cs1;
+
+            inv1.run();
+
+            inv1.start = css1;
+            inv1.run();
+
+            //other test
+            FakeUI f2 = new FakeUI();
+            Console.WriteLine(f2.GetHashCode());
+
+        }
+    }
+}
